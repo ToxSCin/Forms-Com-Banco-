596d7b71fec8029e0585d37cabc614d253d6da59
Tox <renatoserrano.am@gmail.com>
2022-09-28 12:11:12 -0300

    Adicioando Novo Forms "Cidade"

---
+++ 211073/Models/Cidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace _211073.Models
+{
+    public class Cidade
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public string uf { get; set; }
+    }
+
+}
