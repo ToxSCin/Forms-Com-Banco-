72ebd7b5127b4bc7d7863daeff4e85b80ae4e84d
Tox <renatoserrano.am@gmail.com>
2022-11-09 11:01:26 -0300

    Adicionando View/Models a +

---
+++ 211073/Models/VendaCab.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace _211073.Models
+{
+    public class VendaCab : ContentPage
+    {
+        public VendaCab()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
